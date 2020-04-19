9e6dcd3bd4daee3df92dd91d61448bbe9e68437d
Sev Gassauer-Fleissner <severin.gassauer@gmail.com>
2020-04-19 16:32:08 +0000

    Magic sorting up and down

--- api/stockdata.py
+++ api/stockdata.py
@@ -3,12 +3,19 @@ from stockdata_service import stockdata_service
 import logging
 import database.base
 
-sd = stockdata_service.StockDataService(db_session=database.base.db_session)
+logger = logging.getLogger(__name__)
 
+logger.setLevel(logging.DEBUG)
+
+#breakpoint()
+
+sd = stockdata_service.StockDataService(db_session=database.base.db_session)
+logger.debug(f"I just created a new session and who knows why?")
+logger.debug(f"HELLO FROM{__name__} ")
 # Handler for getting a ticker
 def get(ticker, force_refresh):
     # Invoke the stock data extractor
-    logging.getLogger(__name__).debug(
+    logger.debug(
         f"From the handler Request for {ticker} {force_refresh}"
     )
     return sd.get_ticker(ticker=ticker, refresh=force_refresh)
